e00465e2caf551855f936e20404b5ec8277f5cd9
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2020-11-04 14:45:58 +0300

    Add files via upload

---
+++ py_example5.py
@@ -0,0 +1,16 @@
+#Rümeysa Coşkun
+#Python basit bir variables örneği
+x=3
+y=2
+z="Hi,dikey"
+
+if x > y:
+    print("Xbigger than from Y")
+if y > x:
+    print("Y bigger than x ")
+if x == y:
+    print("X equal to Y")
+
+print(x)
+print(y)
+print(z)
